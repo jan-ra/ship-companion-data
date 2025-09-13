99f7e9eb05e6ad0fb9bcf8f3cd4c349b589a480d
Jan Ramdohr <jan.ramdohr@outlook.com>
2025-09-13 14:28:09 +0200

    update css basepath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/ship-companion-data',
+  assetPrefix: '/ship-companion-data',
   images: {
     unoptimized: true
   }
